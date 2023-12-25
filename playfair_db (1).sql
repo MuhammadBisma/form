04f9f6161914fec253fa911c6938fd20873d0a2e
Muhammad Bisma <bismaputra1986@gmail.com>
2023-12-25 18:07:30 +0700

    first commit

--- playfair_db (1).sql
+++ playfair_db (1).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 24, 2023 at 08:35 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Waktu pembuatan: 25 Des 2023 pada 12.06
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `data`
+-- Struktur dari tabel `data`
 --
 
 CREATE TABLE `data` (
@@ -32,23 +32,24 @@ CREATE TABLE `data` (
   `nama` varchar(512) DEFAULT NULL,
   `email` varchar(512) DEFAULT NULL,
   `alamat` varchar(512) DEFAULT NULL,
-  `no_telp` varchar(512) DEFAULT NULL
+  `no_telp` varchar(512) DEFAULT NULL,
+  `data_awal` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `data`
+-- Dumping data untuk tabel `data`
 --
 
-INSERT INTO `data` (`id_integer`, `nama`, `email`, `alamat`, `no_telp`) VALUES
-('452992', 'OPX8', 'OPX8MBNCBO4M', 'H4N8F02PH02P', 'PO3QYMSIQD3LLFRY39GSDD'),
-('597293', 'OPX4', 'OPX4M1N71ODN', 'HFN4CA3P5A3P', 'POBQYMSIQ8BLL5RYDFGS88');
+INSERT INTO `data` (`id_integer`, `nama`, `email`, `alamat`, `no_telp`, `data_awal`) VALUES
+('530993', 'OP1L', 'OP1LMALGAOEN', 'KG4LONHLGH', 'YDTMOHOCBOHIUT4YCOTCY4OSYMBOHIUUYMBMQS', '{\"nama\":\"noval\",\"email\":\"noval12@gmail.com\",\"alamat\":\"jalan mana ge\",\"no_telp\":\"098772824\"}'),
+('84bd18', '2HRN', '2HRNF8LF8O4M', '8210ONBLFF82', 'PO7SYMSIS66BPTF68YHBHYBHX09HKHIS', '{\"nama\":\"bisma\",\"email\":\"bisma1@gmail.com\",\"alamat\":\"jalan mana aja\",\"no_telp\":\"12345678\"}');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `data`
+-- Indeks untuk tabel `data`
 --
 ALTER TABLE `data`
   ADD PRIMARY KEY (`id_integer`);
